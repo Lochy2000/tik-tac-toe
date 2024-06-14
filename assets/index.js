79206444786907d6a0687deaa731d5b9005e3626
lochy2000 <lochlann_oht@homtail.com>
2024-06-14 15:35:54 +0100

    updates player display

--- assets/index.js
+++ assets/index.js
@@ -17,16 +17,36 @@ window.addEventListener('DOMcontentnLoaded', () => {
     [3] [4] [5]
     [6] [7] [8]
 */
-const winningConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-];
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
+    const changePlayer = () => {
+        playerDispaly.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDispaly.innerText = currentPlayer;
+        playerDispaly.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerTExt = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`)
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+
+    tiles.forEach((tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
     resetButton.addEventListener('click', resetBoard);
 });
