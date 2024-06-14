a03e8672b641da0e7fe60425c3b5901200f44c0b
lochy2000 <lochlann_oht@homtail.com>
2024-06-14 15:20:12 +0100

    added event listener for each tile on the array

--- assets/index.js
+++ assets/index.js
@@ -4,10 +4,29 @@ window.addEventListener('DOMcontentnLoaded', () => {
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer')
 
-    let board = ('','','','','','','','','');
+    let board = ('', '', '', '', '', '', '', '', '');
     let currentPlayer = 'x';
     let isGameActive = true;
 
-    resetButton.addEventListener('click', resetBoard);
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
 
+    /* Indexes within the board
+    [0] [1] [2]
+    [3] [4] [5]
+    [6] [7] [8]
+*/
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+    resetButton.addEventListener('click', resetBoard);
 });
